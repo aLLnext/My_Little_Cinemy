f5659fe998340acec3cfc2ca92ce1f9f3a0ed358
Jackob <abdurashidov.aak@students.dvfu.ru>
2018-12-16 00:53:25 +1000

    Added @Query

--- src/main/java/My_Little_Teamy/My_Little_Cinemy/repos/DBWorkRepo.java
+++ src/main/java/My_Little_Teamy/My_Little_Cinemy/repos/DBWorkRepo.java
@@ -1,7 +1,12 @@
 package My_Little_Teamy.My_Little_Cinemy.repos;
 
 import My_Little_Teamy.My_Little_Cinemy.domain.DBWork;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Collection;
+
 public interface DBWorkRepo extends CrudRepository<DBWork, Integer> {
+    @Query(value = "SELECT username FROM user", nativeQuery = true)
+    Collection<String> findAllActiveUsers();
 }
