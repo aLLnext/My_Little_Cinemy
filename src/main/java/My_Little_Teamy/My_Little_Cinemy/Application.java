1809dbe8fa84dc7e85a68e4f2bbbadb4a5f1199d
Jackob <abdurashidov.aak@students.dvfu.ru>
2019-01-02 23:39:00 +1000

    Added JPA

--- src/main/java/My_Little_Teamy/My_Little_Cinemy/Application.java
+++ src/main/java/My_Little_Teamy/My_Little_Cinemy/Application.java
@@ -2,8 +2,10 @@ package My_Little_Teamy.My_Little_Cinemy;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
+@EnableJpaRepositories
 public class Application {
 
     public static void main(String[] args) {
