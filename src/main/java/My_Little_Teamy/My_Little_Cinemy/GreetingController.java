9be7a8f784117e2ccb557f075c6d4e04030a5f31
Jackob <abdurashidov.aak@students.dvfu.ru>
2019-01-02 23:47:23 +1000

    Update `GreetingController`

--- src/main/java/My_Little_Teamy/My_Little_Cinemy/GreetingController.java
+++ src/main/java/My_Little_Teamy/My_Little_Cinemy/GreetingController.java
@@ -1,29 +1,32 @@
 package My_Little_Teamy.My_Little_Cinemy;
 
-import My_Little_Teamy.My_Little_Cinemy.domain.DBWork;
-import My_Little_Teamy.My_Little_Cinemy.repos.DBWorkRepo;
+import My_Little_Teamy.My_Little_Cinemy.domain.Book;
+import My_Little_Teamy.My_Little_Cinemy.domain.User;
+import My_Little_Teamy.My_Little_Cinemy.repos.BookRepo;
+import My_Little_Teamy.My_Little_Cinemy.repos.UserRepo;
+import lombok.AllArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-import java.util.Collection;
 import java.util.Map;
 
 @Controller
+@AllArgsConstructor(onConstructor = @__(@Autowired))
 public class GreetingController {
 
     @Autowired
-    private DBWorkRepo dbWork;
+    private UserRepo userRepo;
+    @Autowired
+    private BookRepo bookRepo;
     @GetMapping("/greeting")
     public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name,
                            Map<String, Object> model) {
-        Collection<String> test = dbWork.findAllActiveUsers();
-        model.put("users", test);
-
-        Integer a = 0;
+        Iterable<User> test = userRepo.findAll();
+        Iterable<Book> test2 = bookRepo.findAll();
+//        model.put("test", test);
+        int a=8;
         return "greeting";
     }
 
