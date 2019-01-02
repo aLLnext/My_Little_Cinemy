6ac167155fe73efb36fa4f428ebe42ecbf45dd77
Jackob <abdurashidov.aak@students.dvfu.ru>
2019-01-03 01:22:51 +1000

    Added `Country` entity with interface for auto-generated SQL queries

---
+++ src/main/java/My_Little_Teamy/My_Little_Cinemy/repos/CountryRepo.java
@@ -0,0 +1,7 @@
+package My_Little_Teamy.My_Little_Cinemy.repos;
+
+import My_Little_Teamy.My_Little_Cinemy.domain.County;
+import org.springframework.data.repository.CrudRepository;
+
+public interface CountryRepo extends CrudRepository<County, Long> {
+}
