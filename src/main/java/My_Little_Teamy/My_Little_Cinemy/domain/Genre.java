0a11d87a4dbfc36dd6715487061aa90c9f0e4ec7
1Jackob1 <saxolita@ya.ru>
2019-01-03 15:51:12 +1000

    Added fully entity for `Genre`

---
+++ src/main/java/My_Little_Teamy/My_Little_Cinemy/domain/Genre.java
@@ -0,0 +1,19 @@
+package My_Little_Teamy.My_Little_Cinemy.domain;
+
+import lombok.Data;
+import org.springframework.data.annotation.TypeAlias;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Data
+@Entity
+@Table(name = "genres")
+public class Genre {
+    @Id
+    @GeneratedValue
+    private long id;
+    private String name;
+}
