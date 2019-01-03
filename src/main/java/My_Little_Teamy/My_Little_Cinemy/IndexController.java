445bbdeb9d8bc14f014e69d128c23857d070f998
Anton Mikhailenko <toxaxab27@gmail.com>
2019-01-03 18:41:19 +1000

    Add film preview
    •Add film preview on index page
    •Small refactoring and fixes

--- src/main/java/My_Little_Teamy/My_Little_Cinemy/IndexController.java
+++ src/main/java/My_Little_Teamy/My_Little_Cinemy/IndexController.java
@@ -5,6 +5,7 @@ import My_Little_Teamy.My_Little_Cinemy.domain.User;
 import My_Little_Teamy.My_Little_Cinemy.repos.BookRepo;
 import My_Little_Teamy.My_Little_Cinemy.repos.UserRepo;
 import lombok.AllArgsConstructor;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -13,21 +14,25 @@ import org.springframework.web.bind.annotation.RequestParam;
 import java.util.Map;
 
 @Controller
-@AllArgsConstructor(onConstructor = @__(@Autowired))
-public class GreetingController {
+@AllArgsConstructor(onConstructor=@__(@Autowired))
+public class IndexController {
 
     @Autowired
     private UserRepo userRepo;
     @Autowired
     private BookRepo bookRepo;
-    @GetMapping("/greeting")
+    @GetMapping("/index")
     public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name,
                            Map<String, Object> model) {
-        Iterable<User> test = userRepo.findAll();
-        Iterable<Book> test2 = bookRepo.findAll();
+        //Iterable<User> test = userRepo.findAll();
+        //Iterable<Book> test2 = bookRepo.findAll();
 //        model.put("test", test);
         int a=8;
-        return "greeting";
+        return "index";
     }
 
+    @GetMapping("/account")
+    public String account(){
+        return "account";
+    }
 }
--- src/main/java/My_Little_Teamy/My_Little_Cinemy/repos/UserRepo.java
+++ src/main/java/My_Little_Teamy/My_Little_Cinemy/repos/UserRepo.java
@@ -1,6 +1,8 @@
 package My_Little_Teamy.My_Little_Cinemy.repos;
 
 import My_Little_Teamy.My_Little_Cinemy.domain.User;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 public interface UserRepo extends CrudRepository<User, Long> {
