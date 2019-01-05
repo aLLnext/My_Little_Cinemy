9cf16ab7f1558f51684b62e48192f7f4d2607182
Anton Mikhailenko <toxaxab27@gmail.com>
2019-01-05 14:27:08 +1000

    Add checking Error

--- src/main/java/My_Little_Teamy/My_Little_Cinemy/FrontController.java
+++ src/main/java/My_Little_Teamy/My_Little_Cinemy/FrontController.java
@@ -8,6 +8,7 @@ import My_Little_Teamy.My_Little_Cinemy.repos.UserRepo;
 import lombok.AllArgsConstructor;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -16,11 +17,12 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.servlet.RequestDispatcher;
 import javax.servlet.http.HttpServletRequest;
 import java.util.Map;
 
 @Controller
-@AllArgsConstructor(onConstructor=@__(@Autowired))
+@AllArgsConstructor(onConstructor = @__(@Autowired))
 public class FrontController {
 
     @Autowired
@@ -30,14 +32,32 @@ public class FrontController {
     @Autowired
     private FilmRepo films;
     public static final String POST_REQUEST_INDEX_NAME = "index";
+
     @RequestMapping(value = "/index", method = RequestMethod.GET)
-    public ModelAndView post_request_index(HttpServletRequest request){
+    public ModelAndView post_request_index(HttpServletRequest request) {
         ModelAndView result = new ModelAndView();
         result.addObject("title", "ВАХОБ ЗОЕБАЛ");
         result.addObject("films_titles", films.getFilmsTitles());
         result.addObject("films_pic", films.getFilmsImages());
         return result;
     }
+
+    @RequestMapping(value = "/error")
+    public String handleError(HttpServletRequest request) {
+        Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+
+        if (status != null) {
+            Integer statusCode = Integer.valueOf(status.toString());
+
+            if (statusCode == HttpStatus.NOT_FOUND.value()) {
+                return "error-404";
+            } else if (statusCode == HttpStatus.INTERNAL_SERVER_ERROR.value()) {
+                return "error-500";
+            }
+        }
+        return "error";
+    }
+
     /*public String greeting(@RequestParam(name="name", required=false, defaultValue="World") String name,
                            Map<String, Object> model) {
         //Iterable<User> test = userRepo.findAll();
@@ -50,7 +70,7 @@ public class FrontController {
     }*/
 
     @RequestMapping("/account")
-    public String account(){
+    public String account() {
         return "account";
     }
 }
