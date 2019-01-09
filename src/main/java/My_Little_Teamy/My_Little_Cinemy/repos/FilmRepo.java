1cb567d3d9df7f1393c55e09730045bac5729959
Jackob <abdurashidov.aak@students.dvfu.ru>
2019-01-10 00:26:30 +1000

    Optimize imports

--- src/main/java/My_Little_Teamy/My_Little_Cinemy/repos/FilmRepo.java
+++ src/main/java/My_Little_Teamy/My_Little_Cinemy/repos/FilmRepo.java
@@ -1,10 +1,7 @@
 package My_Little_Teamy.My_Little_Cinemy.repos;
 
 import My_Little_Teamy.My_Little_Cinemy.domains.Film;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
-
-import java.util.List;
 
 public interface FilmRepo extends CrudRepository<Film, Long> {
     Film findFilmById(long id);
