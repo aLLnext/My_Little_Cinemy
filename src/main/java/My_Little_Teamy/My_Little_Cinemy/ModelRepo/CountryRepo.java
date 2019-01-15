a7ea1853b1256938bc53ff75b4ee4908ec2ac023
Jackob <abdurashidov.aak@students.dvfu.ru>
2019-01-15 16:03:18 +1000

    Added findCountriesByFilmId

--- src/main/java/My_Little_Teamy/My_Little_Cinemy/ModelRepo/CountryRepo.java
+++ src/main/java/My_Little_Teamy/My_Little_Cinemy/ModelRepo/CountryRepo.java
@@ -1,7 +1,12 @@
 package My_Little_Teamy.My_Little_Cinemy.ModelRepo;
 
 import My_Little_Teamy.My_Little_Cinemy.Model.Country;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 public interface CountryRepo extends CrudRepository<Country, Long> {
+    @Query(value = "select name from countries inner join films_countries on " +
+            "countries.id = country_id where film_id = :filmId", nativeQuery = true)
+    Iterable<String> findCountriesByFilmId(@Param("filmId") Long filmId);
 }
