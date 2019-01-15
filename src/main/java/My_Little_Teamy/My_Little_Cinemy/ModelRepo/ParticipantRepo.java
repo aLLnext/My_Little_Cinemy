61a5351202c1780f32756d7a4f4307edff95a060
Jackob <abdurashidov.aak@students.dvfu.ru>
2019-01-15 16:23:24 +1000

    Added findPartisipantsByfilmIdAndPost

--- src/main/java/My_Little_Teamy/My_Little_Cinemy/ModelRepo/ParticipantRepo.java
+++ src/main/java/My_Little_Teamy/My_Little_Cinemy/ModelRepo/ParticipantRepo.java
@@ -1,7 +1,15 @@
 package My_Little_Teamy.My_Little_Cinemy.ModelRepo;
 
 import My_Little_Teamy.My_Little_Cinemy.Model.Participant;
+import javafx.util.Pair;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 public interface ParticipantRepo extends CrudRepository<Participant, Long> {
+    @Query(value = "select name, surname from people inner join films_people on " +
+            "people.id = participant_id where (film_id = :filmId and post = :post)",
+            nativeQuery = true)
+    Iterable<Iterable<String>> findPartisipantsByfilmIdAndPost(@Param("filmId") Long filmId,
+                                                                   @Param("post") String post);
 }
