9e5b68985158b246f1205996f27b497d049c7269
Jackob <abdurashidov.aak@students.dvfu.ru>
2019-01-15 15:51:38 +1000

    update naming

--- src/main/java/My_Little_Teamy/My_Little_Cinemy/ModelRepo/GenreRepo.java
+++ src/main/java/My_Little_Teamy/My_Little_Cinemy/ModelRepo/GenreRepo.java
@@ -8,5 +8,5 @@ import org.springframework.data.repository.query.Param;
 public interface GenreRepo extends CrudRepository<Genre, Long> {
     @Query(nativeQuery = true, value = "select name from genres inner join films_genres on " +
             "genres.id = genre_id where film_id = :filmId")
-    Iterable<String> findGenresByFilm(@Param("filmId") Long filmId);
+    Iterable<String> findGenresByFilmId(@Param("filmId") Long filmId);
 }
