9ae8185d5fd1f22fd0a83833e5580a5dcd74d7dc
Jackob <abdurashidov.aak@students.dvfu.ru>
2019-01-15 15:51:14 +1000

    Added findGenresByFilmId method

--- src/main/java/My_Little_Teamy/My_Little_Cinemy/ModelRepo/GenreRepo.java
+++ src/main/java/My_Little_Teamy/My_Little_Cinemy/ModelRepo/GenreRepo.java
@@ -1,7 +1,12 @@
 package My_Little_Teamy.My_Little_Cinemy.ModelRepo;
 
 import My_Little_Teamy.My_Little_Cinemy.Model.Genre;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 public interface GenreRepo extends CrudRepository<Genre, Long> {
+    @Query(nativeQuery = true, value = "select name from genres inner join films_genres on " +
+            "genres.id = genre_id where film_id = :filmId")
+    Iterable<String> findGenresByFilm(@Param("filmId") Long filmId);
 }
